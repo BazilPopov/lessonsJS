ccf92998ab5c8674da70884b8ebea0242d0eb1d9
Bazil Popow <bazilpopow94@gmail.com>
2019-02-24 05:54:02 +0300

    все дз

---
+++ 1/js/js.js
@@ -0,0 +1,31 @@
+var budget = +prompt("Ваш бюджет:");
+console.log(budget);
+var namemag = prompt("Название Вашего магазина:");
+console.log(namemag);
+var arr = [];
+arr[0] = prompt("Наименование товара:");
+arr[1] = prompt("Наименование товара:");
+arr[2] = prompt("Наименование товара:");
+console.log(arr);
+
+
+var mainList = obj  = {
+	budg: budget,
+	name: namemag,
+	shopGoods: arr,
+	employers: {},
+	open: false
+};
+
+alert(mainList.budg / 30);
+
+console.log(mainList);
+
+
+
+var num = 33721;
+var num1 = 3*3*7*2*1;
+var num2 = num1**3;
+console.log(num);
+console.log(num1);
+console.log(num2);
---
+++ 2/js.js
@@ -0,0 +1,39 @@
+var budget = +prompt("Ваш бюджет:");
+console.log(budget);
+var namemag = prompt("Название Вашего магазина:");
+console.log(namemag);
+var arr = [];
+for (let i=0; i<2; i++){
+	let a = prompt("Название товара:");
+	if ((typeof(a)) === 'string' && a != '' && a.length<50){
+		arr[i] = a;
+	}else{
+		alert("Вы ввели неверное значение повторите пожалуйста!");
+		i--;
+	}
+}
+
+
+console.log(arr);
+
+
+var mainList = obj  = {
+	budg: budget,
+	name: namemag,
+	shopGoods: arr,
+	employers: {},
+	open: false
+};
+
+alert(mainList.budg / 30);
+
+console.log(mainList);
+
+
+
+var num = 33721;
+var num1 = 3*3*7*2*1;
+var num2 = num1**3;
+console.log(num);
+console.log(num1);
+console.log(num2);
